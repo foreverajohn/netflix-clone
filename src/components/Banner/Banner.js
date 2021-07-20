40d6154984f4dcbf29c1219e30bf5c05d6f12ea7
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-07-20 13:56:05 -0500

    axios and tmdb API setup

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,22 +2,27 @@ import React from 'react'
 import './Banner.css'
 
 const Banner = () => {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
     return (
         <header
             className='banner'
             style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
+                backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
                 backgroundPosition: 'center center',
             }}
         >
             <div className="banner__contents">
                 <h1 className='banner__title'>Movie Name</h1>
                 <div className="banner__buttons">
-                    <button className="banner__buttons">Play</button>
-                    <button className="banner__buttons">My List</button>
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
                 </div>
-                <h1 className="banner__description">Test description</h1>
+                <h1 className="banner__description">{truncate(`This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 150)}</h1>
             </div>
             <div className="banner--fadeBottom" />
         </header>
