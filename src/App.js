fe9ca140d5e02aa386324509d3ed6f746ba2ee78
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-07-20 19:52:22 -0500

    sign in functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
+  const user = null
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+
+      }
+    })
+    return unsubscribe
+  }, [])
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
+
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC2bxKYM0VJgV29dBSMIxZ7XD3AGMYLqM8",
-    authDomain: "netflix-a2ac2.firebaseapp.com",
-    projectId: "netflix-a2ac2",
-    storageBucket: "netflix-a2ac2.appspot.com",
-    messagingSenderId: "238660685444",
-    appId: "1:238660685444:web:3a951dcb512b0c953ffcd1"
-  };
+  apiKey: "AIzaSyC2bxKYM0VJgV29dBSMIxZ7XD3AGMYLqM8",
+  authDomain: "netflix-a2ac2.firebaseapp.com",
+  projectId: "netflix-a2ac2",
+  storageBucket: "netflix-a2ac2.appspot.com",
+  messagingSenderId: "238660685444",
+  appId: "1:238660685444:web:3a951dcb512b0c953ffcd1"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth }
+export default db
